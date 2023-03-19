899ba874ec04971d773f3b4246ed7b4cd6e08131
KABANGA David <Davkabanga96@gmail.com>
2023-03-19 22:14:36 +0200

    made some changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
+
+function Navbar() {
+    return <h1>Hello world, I am linked</h1>
+    
+}
+
+export default Navbar;
